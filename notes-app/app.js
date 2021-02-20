d810f59c05559fadf5d488d205b375f6d7a52e87
AntonMakarkin <amakarkin27@gmail.com>
2021-02-20 19:44:08 +0300

    Lesson 15. Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,15 +2,22 @@ const validator = require('validator');
 const getNotes = require('./notes');
 const chalk = require('chalk');
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-console.log(validator.isEmail('amakarkin27.com'));
-console.log(validator.isURL('https:/meas.io'));
-console.log(chalk.green.bold("Success!"));
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding note!');
+} else if (command === 'remove') {
+    console.log('Removing note!');
+}
 
 
+//const msg = getNotes();
+//console.log(msg);
 
-/*const add = require('./utils');
-const sum = add(4, -2);
-console.log(sum);*/
+//console.log(validator.isEmail('amakarkin27.com'));
+//console.log(validator.isURL('https:/meas.io'));
+//console.log(chalk.green.bold("Success!"));
+
+//console.log(process.argv[2]);
