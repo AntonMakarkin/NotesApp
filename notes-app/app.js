47c93479df4b345068df2214fd8b58c7f05e0aa3
AntonMakarkin <amakarkin27@gmail.com>
2021-03-01 14:54:37 +0300

    Lesson 19. Adding a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,4 @@
-const validator = require('validator');
-const getNotes = require('./notes');
+const notes = require('./notes');
 const chalk = require('chalk');
 const yargs = require('yargs');
 
@@ -23,8 +22,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
