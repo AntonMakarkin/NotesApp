fe3ea97164f977065e9c58fcd00f031f97ba9a47
AntonMakarkin <amakarkin27@gmail.com>
2021-03-09 16:01:54 +0300

    Lesson 32. Customazing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
-const request = require('request');
+import request from 'request';
 
 const url = 'http://api.weatherstack.com/current?access_key=f8626bd7c4b5b714577a053ee790f719&query=37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    //console.log(response.body.current);
+    let res = response.body.current;
+    console.log(`${res.weather_descriptions[0]}. It is currently ${res.temperature} degress out. It feels like ${res.feelslike} degress out`);
 });
