08aeff0afba2e4c8031aba959cf7361741113608
AntonMakarkin <amakarkin27@gmail.com>
2021-03-12 16:00:38 +0300

    Lesson 38-39. Callback chaining, ES6 Aside: Object Property Shorthand and Destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,24 @@
 import geocode from './utils/geocode.js';
 import forecast from './utils/forecast.js';
 
-geocode('Saint Petersburg', (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data);
-})
+const address = process.argv[2]
+//const address = '';
 
-forecast(59.95, 30.31667, (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data);
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
