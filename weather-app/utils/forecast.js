fc5707ecd616120e785f374348c97091a3c7f2a1
AntonMakarkin <amakarkin27@gmail.com>
2021-03-13 01:40:50 +0300

    Lesson 40. Destructuring and Property Shorthand Challenge

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,14 +3,13 @@ import request from 'request';
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=f8626bd7c4b5b714577a053ee790f719&query=${latitude},${longitude}`;
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            let res = response.body.current
-            callback(undefined, `${res.weather_descriptions[0]}. It is currently ${res.temperature} degress out. It feels like ${res.feelslike} degress out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out`)
         }
     })
 }
