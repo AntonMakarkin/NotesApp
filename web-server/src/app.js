70d9de94f6728ebb14952f2bd6c874039004d08d
AntonMakarkin <amakarkin27@gmail.com>
2021-03-16 22:54:35 +0300

    Lesson 44. Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ import express from 'express';
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send({
+        name: 'Anton',
+        age: 20
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About Page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Moscow'
+    })
 })
 
 app.listen(3000, () => {
