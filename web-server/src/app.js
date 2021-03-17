eff1a119b4d26d348c6f9e48ea85c39c76e52b41
AntonMakarkin <amakarkin27@gmail.com>
2021-03-17 17:49:39 +0300

    Lesson 45. Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,18 @@
-import express from 'express';
+const path = require('path');
+const express = require('express')
+/*import express from 'express'
+import path from 'path'*/
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const pathToAboutPage = path.join(__dirname, '../public/about.html')
+const pathToHelpPage = path.join(__dirname, '../public/help.html')
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath)) //load at first
+app.use(express.static(pathToAboutPage))
+app.use(express.static(pathToHelpPage))
 
-app.get('/help', (req, res) => {
+/*app.get('/help', (req, res) => {
     res.send({
         name: 'Anton',
         age: 20
@@ -15,7 +21,7 @@ app.get('/help', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('<h1>About Page</h1>')
-})
+})*/
 
 app.get('/weather', (req, res) => {
     res.send({
