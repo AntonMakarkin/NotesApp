a5c34064dc5fce259d77114d35928c97fa7e734d
AntonMakarkin <amakarkin27@gmail.com>
2021-03-17 22:38:52 +0300

    Lesson 46. Serving up CSS,JS,Images and more

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,13 @@
 import express from 'express'
 import path from 'path'
-import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-const pathToAboutPage = path.join(__dirname, '../public/about.html')
-const pathToHelpPage = path.join(__dirname, '../public/help.html')
 
 app.use(express.static(publicDirectoryPath)) //load at first
-app.use(express.static(pathToAboutPage))
-app.use(express.static(pathToHelpPage))
-
-/*app.get('/help', (req, res) => {
-    res.send({
-        name: 'Anton',
-        age: 20
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>')
-})*/
 
 app.get('/weather', (req, res) => {
     res.send({
