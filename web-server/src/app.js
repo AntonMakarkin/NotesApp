8dfcf9ef3e4b43b81783a72dad6d325901543297
AntonMakarkin <amakarkin27@gmail.com>
2021-03-17 23:32:43 +0300

    Lesson 47. Dynamic Pages with Templating, using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,7 +7,29 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine','hbs') //adding settings
 app.use(express.static(publicDirectoryPath)) //load at first
+
+app.get('/', (req, res) => { 
+    res.render('index', { //get view and converts it inti html
+        title: 'Weather',
+        name: 'Anton Makarkin'
+    }) 
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Anton Makarkin'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpMessage: 'Do you need help? Check this page and find an answer'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
