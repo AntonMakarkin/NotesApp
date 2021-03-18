f398e48b354d98631964ca5f3a76b531abce1fb9
AntonMakarkin <amakarkin27@gmail.com>
2021-03-18 16:24:00 +0300

    Lesson 50. 404 Pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -49,6 +49,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anton Makarkin',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anton Makarkin',
+        errorMessage: 'Page not found'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 });
