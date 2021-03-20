d563b5f3c891249c40c187f41cac768c7e28f698
AntonMakarkin <amakarkin27@gmail.com>
2021-03-20 15:30:56 +0300

    Lesson 57. Browser HTTP Requests with Fetch

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -63,7 +63,7 @@ app.get('/weather', (req, res) => {
 
             res.send({
                 forecast: forecastData,
-                location: location,
+                location,
                 address: req.query.address
             })
         })
