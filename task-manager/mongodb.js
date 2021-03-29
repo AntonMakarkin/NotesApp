f7ac0bd4fb83b32f71e7f2722d6567752a6510e3
AntonMakarkin <amakarkin27@gmail.com>
2021-03-29 15:33:33 +0300

    Lesson 81. Deleting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,29 +14,18 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true}, { useNewUrlParse
 
     const db = client.db(databaseName)
 
-    /*db.collection('users').updateOne({
-        _id: new ObjectID("605dcf5c6ae5f93c580a6bdf")
-    }, {
-        $set: {
-            name: 'Mike'
-        },
-        $inc: {
-            age: 1
-        }
+    /*db.collection('users').deleteMany({
+        age: 21
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
         console.log(error)
     })*/
 
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
+    db.collection('tasks').deleteOne({
+        description: "Go in the university to the first lesson"
     }).then((result) => {
-        console.log(result.modifiedCount)
+        console.log(result)
     }).catch((error) => {
         console.log(error)
     })
