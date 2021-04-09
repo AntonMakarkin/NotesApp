b6792e0669c53d5423d5eda0a294e0d6c135efab
AntonMakarkin <amakarkin27@gmail.com>
2021-04-09 16:14:51 +0300

    Lesson 103. Securely Storing Passwords: Part 1

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import DBconnect from './db/mongoose.js'
 import userRouter from './routers/users.js'
 import taskRouter from './routers/tasks.js'
+import bcrypt from 'bcryptjs'
 
 DBconnect()
 
@@ -16,3 +17,16 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('red12345!', hashedPassword) 
+    console.log(isMatch)
+}
+
+myFunction()
