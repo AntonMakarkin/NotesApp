6a678a10b1c1886b08846371f7b440e6a1d6f3bc
AntonMakarkin <amakarkin27@gmail.com>
2021-04-26 16:13:50 +0300

    Lesson 138. Writing Tests and Assertions

--- playground/7-default-params.js
+++
@@ -1,6 +0,0 @@
-const greeter = (name = 'user', age = 18) => {
-    console.log('Hello ' + name, 'age ' + age)
-}
-
-greeter('Anton')
-greeter()
---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,16 @@
+const { calculateTip } = require('../src/math')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+
+    /*if (total !== 13) {
+        throw new Error('Total tip should be 13. Got ' + total)
+    }*/
+
+})
+
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
