531d2da018d909b0a962e3f0bc1de662147e886e
AntonMakarkin <amakarkin27@gmail.com>
2021-05-01 23:40:53 +0300

    Lesson 158. Sharing Your Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -4,12 +4,28 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
+
 document.querySelector('#message-form').addEventListener('submit', (event) => {
     event.preventDefault()
 
     const message = event.target.elements.message.value
 
     socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+
 })
 
 /*document.querySelector('#increment').addEventListener('click', () => {
