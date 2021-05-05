d5931da24ddd502f8d6e4ae60eaaab69760fa2ed
AntonMakarkin <amakarkin27@gmail.com>
2021-05-05 15:51:20 +0300

    Lesson 163. Working with time

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -14,7 +14,8 @@ const locationTemplate = document.querySelector('#location-template').innerHTML
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('H:mm')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
